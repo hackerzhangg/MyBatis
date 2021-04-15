7c6e076af85499764ec6463c2a7b0132209eb361
zgp <2088220905@qq.com>
2021-04-15 16:34:53 +0800

    MybatisStart

---
+++ Mybatis-Start/src/main/java/com/bigjava/mybatis/Test/TestMybatis.java
@@ -0,0 +1,29 @@
+package com.bigjava.mybatis.Test;
+
+import com.bigjava.mybatis.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+//import javax.annotation.Resource;
+
+/**
+ * @Author zgp
+ * @Since 2021 -04 -15 16 :16
+ * @Description
+ */
+public class TestMybatis {
+
+    public static void main(String[] args) throws IOException {
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+        SqlSession sqlSession = factory.openSession();
+        List<User> userList = sqlSession.selectList("com.bigjava.mybatis.mapper.UserMapper.getUserAll");
+        for (User user: userList){
+            System.out.println(user.getId()+":"+user.getPassword()+":"+user.getUsername());
+        }
+    }
+}
---
+++ Mybatis-Start/src/main/java/com/bigjava/mybatis/model/User.java
@@ -0,0 +1,46 @@
+package com.bigjava.mybatis.model;
+
+/**
+ * @Author zgp
+ * @Since 2021 -04 -15 11 :30
+ * @Description User实体类模型
+ */
+public class User {
+
+    public int id;
+    public String username;
+    public String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

fe8ad47e71cb22bde549eda6aefc18506629cfa9
zgp <2088220905@qq.com>
2021-04-22 15:08:43 +0800

    MybatisStart

--- Mybatis-Start/src/main/java/com/bigjava/mybatis/test/TestMybatis.java
+++ Mybatis-Start/src/main/java/com/bigjava/mybatis/test/TestMybatis.java
@@ -1,11 +1,9 @@
-package com.bigjava.mybatis.Test;
+package com.bigjava.mybatis.test;
 
 import com.bigjava.mybatis.common.SqlSessionFactoryUtils;
 import com.bigjava.mybatis.model.User;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.util.List;
